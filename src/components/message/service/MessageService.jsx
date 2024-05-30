77c2e3c6190a7c4f4c9fc9d917c268bf232926ac
alberto-sclocchi <scloks75tv@gmail.com>
2024-05-29 23:52:43 -0400

    change baseURL

--- src/components/message/service/MessageService.jsx
+++ src/components/message/service/MessageService.jsx
@@ -3,7 +3,8 @@ import axios from 'axios'
 export default class MessageService {
     constructor(){
         let service = axios.create({
-            baseURL: `http://localhost:5005/message`,
+            // baseURL: `http://localhost:5005/message`,
+            baseURL:'https://portfolio-website-api.fly.dev/message',
             withCredentials: false
         })
 
