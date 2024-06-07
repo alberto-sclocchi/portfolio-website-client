c8797f8f804a92330954cb2a15e6da56d7a5536f
alberto-sclocchi <scloks75tv@gmail.com>
2024-06-06 22:00:02 -0400

    change pdf url

--- src/components/sections/DownloadResume.jsx
+++ src/components/sections/DownloadResume.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function DownloadResume() {
 
 const handleClick = () => {
-    const pdfUrl = "http://localhost:3000/alberto-sclocchi-resume.pdf"
+    // const pdfUrl = "http://localhost:3000/alberto-sclocchi-resume.pdf"
+    const pdfUrl = "https://alberto-sclocchi-portfolio.netlify.app/alberto-sclocchi-resume.pdf"
     const fileName = pdfUrl.split('/').pop();
     const link = document.createElement("a");
     link.setAttribute('href', pdfUrl);
