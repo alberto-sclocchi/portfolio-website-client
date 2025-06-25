2f9d2db985bcc458e762c8d895b323985c383ecd
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-24 20:47:07 -0400

    create flipping feature with DOM manipulation and useEffect

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import github from '../../images/git-hub-proj.png'
 import soccerImg from '../../images/soccer-img.jpeg'
 import nodeJS from '../../images/node-icon.png'
@@ -14,11 +13,36 @@ import gymImg from '../../images/gym-img.jpeg'
 import marioImg from '../../images/mario-bros.png'
 
 export default function Projects() {
+  const [ flip, setFlip ] = useState(null)
+
+  useEffect(() => {
+    const projects = document.querySelectorAll(".flip-card");
+
+    projects.forEach((project, i) => {
+      project.addEventListener("click", () => {
+        console.log("here", i);
+        projects.forEach((otherProject, index) => {
+          if(project === otherProject){
+            setFlip(index);
+          }
+        })
+      })
+    
+      return () => {
+        projects.forEach((project) => {
+          project.removeEventListener("click", () => {});    
+        })
+      }
+    
+    })
+
+
+  }, [setFlip])
   return (
     <div id="projects">    
       <h2 className="section-title">Projects</h2>  
       <div className="flip-card">
-        <div className="flip-card-inner">
+        <div className={`flip-card-inner ${flip === 0 ? "flipped" : ""}`}>
           <img className="flip-card-front" src={soccerImg} alt="soccer-img"/>
 
           <div className="flip-card-back">
@@ -28,7 +52,7 @@ export default function Projects() {
       </div>
 
       <div className="flip-card">
-        <div className="flip-card-inner">
+        <div className={`flip-card-inner ${flip === 1 ? "flipped" : ""}`}>
           <img className="flip-card-front" src={gymImg} alt="gym-img"/>
 
           <div className="flip-card-back">
@@ -38,7 +62,7 @@ export default function Projects() {
       </div>
 
       <div className="flip-card">
-        <div className="flip-card-inner">
+        <div className={`flip-card-inner ${flip === 2 ? "flipped" : ""}`}>
           <img className="flip-card-front" src={soccerImg} alt="soccer-img"/>
 
           <div className="flip-card-back">
@@ -48,7 +72,7 @@ export default function Projects() {
       </div>
 
       <div className="flip-card">
-        <div className="flip-card-inner">
+        <div className={`flip-card-inner ${flip === 3 ? "flipped" : ""}`}>
           <img className="flip-card-front" src={marioImg} alt="mario-img"/>
 
           <div className="flip-card-back">
