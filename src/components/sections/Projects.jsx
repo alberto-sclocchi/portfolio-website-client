971f4774b16449b9e0d1923e86f5252db8b5efa8
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-25 14:34:23 -0400

    add tech skills on the back of the project card

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -10,7 +10,7 @@ export default function Projects() {
   const [ flip, setFlip ] = useState(null)
 
   useEffect(() => {
-    const projects = document.querySelectorAll(".flip-button");
+    const projects = document.querySelectorAll(".flip-icon");
 
     projects.forEach((project, i) => {
       console.log("here clicking", i)
@@ -41,20 +41,68 @@ export default function Projects() {
           <div className="flip-card-front" style={{background:`url(${soccerImg})`, backgroundPosition:"center", backgroundSize:"cover", backgroundRepeat:"no-repeat"}}>
             <div>
               <Link className='button-link website-link' to="https://project-soccer.onrender.com"  target='_blank'>website</Link>
-              <span className="flip-button"><img src={flipIcon} alt="flip-icon" className='flip-icon'/></span>
+              <img src={flipIcon} alt="flip-icon" className='flip-icon'/>
             </div>
             <Link to="https://github.com/alberto-sclocchi/project-soccer-app-api" target="_blank"><img src={github} alt="github-icon" className='github-icon'/></Link>
           </div>
 
           <div className="flip-card-back">
-            <span className="flip-button"><img src={flipIcon} alt="flip-icon" className='flip-icon'/></span>
-            <h2 className='section-title'>Super Mario Bros</h2>
+            <div>
+              <h2 className='section-title'>KickShop</h2>
+              <span className="flip-button"><img src={flipIcon} alt="flip-icon" className='flip-icon'/></span>
+            </div>
+            <ul>
+              <li>React</li>
+              <li>Express</li>
+              <li>JavaScript</li>
+              <li>MongoDB</li>
+              <li>Mongoose</li>
+              <li>CRUD</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Nodemailer</li>
+              <li>Axios</li>
+              <li>RESTful API</li>
+              <li>Cloudinary</li>
+            </ul>
           </div>
        </div>
       </div>
 
       <div className="flip-card">
         <div className={`flip-card-inner ${flip === 2 ? "flipped" : flip === 3 ? "" : ""}`}>
+          <div className="flip-card-front" style={{background:`url(${gymImg})`, backgroundPosition:"center", backgroundSize:"cover", backgroundRepeat:"no-repeat"}}>
+            <div>
+              <Link className='button-link website-link' to="https://project-gym-app-patient-wildflower-2606.fly.dev" target='_blank'>website</Link>
+              <img src={flipIcon} alt="flip-icon" className='flip-icon'/>
+            </div>
+            <Link to="https://github.com/alberto-sclocchi/project-gym-app" target="_blank"><img src={github} alt="github-icon" className='github-icon'/></Link>
+          </div>
+
+          <div className="flip-card-back">
+            <div>
+              <h2 className='section-title'>FitHub</h2>
+              <img src={flipIcon} alt="flip-icon" className='flip-icon'/>
+            </div>
+            <ul>
+              <li>Express</li>
+              <li>MongoDB</li>
+              <li>Mongoose</li>
+              <li>Express Session</li>
+              <li>Axios</li>
+              <li>Handlebars</li>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Nodemailer</li>
+              <li>Cloudinary</li>
+            </ul>
+          </div>
+       </div>
+      </div>
+
+      <div className="flip-card">
+        <div className={`flip-card-inner ${flip === 4 ? "flipped" : flip === 5 ? "" : ""}`}>
           <div className="flip-card-front" style={{background:`url(${marioImg})`, backgroundPosition:"center", backgroundSize:"cover", backgroundRepeat:"no-repeat"}}>
             <div>
               <Link className='button-link website-link' to="https://alberto-sclocchi.github.io/project-mario-game/" target='_blank'>website</Link>
@@ -64,25 +112,17 @@ export default function Projects() {
           </div>
 
           <div className="flip-card-back">
-            <span className="flip-button"><img src={flipIcon} alt="flip-icon" className='flip-icon'/></span>
-            <h2 className='section-title'>Super Mario Bros</h2>
-          </div>
-       </div>
-      </div>
-
-      <div className="flip-card">
-        <div className={`flip-card-inner ${flip === 4 ? "flipped" : flip === 5 ? "" : ""}`}>
-          <div className="flip-card-front" style={{background:`url(${gymImg})`, backgroundPosition:"center", backgroundSize:"cover", backgroundRepeat:"no-repeat"}}>
             <div>
-              <Link className='button-link website-link' to="https://project-gym-app-patient-wildflower-2606.fly.dev" target='_blank'>website</Link>
-              <span className="flip-button"><img src={flipIcon} alt="flip-icon" className='flip-icon'/></span>
+              <h2 className='section-title'>Super Mario Bros</h2>
+              <img src={flipIcon} alt="flip-icon" className='flip-icon'/>
             </div>
-            <Link to="https://github.com/alberto-sclocchi/project-gym-app" target="_blank"><img src={github} alt="github-icon" className='github-icon'/></Link>
-          </div>
-
-          <div className="flip-card-back">
-            <span className="flip-button"><img src={flipIcon} alt="flip-icon" className='flip-icon'/></span>
-            <h2 className='section-title'>FitHub</h2>
+            <ul>
+              <li>DOM Manipulation</li>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Object Oriented Programming (OOB)</li>
+            </ul>
           </div>
        </div>
       </div>
@@ -92,7 +132,7 @@ export default function Projects() {
           <div className="flip-card-front" style={{background:`url(${gymImg})`, backgroundPosition:"center", backgroundSize:"cover", backgroundRepeat:"no-repeat"}}>
             <div>
               <Link to="#contact-me" className='button-link website-link'>website</Link>
-              <span className="flip-button"><img src={flipIcon} alt="flip-icon" className='flip-icon'/></span>
+              <img src={flipIcon} alt="flip-icon" className='flip-icon'/>
             </div>
             <Link to="#contact-me"><img src={github} alt="github-icon" className='github-icon'/></Link>
           </div>
